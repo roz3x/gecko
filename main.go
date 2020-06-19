5252e4c4830bc203b85df4fe35879bf027c8cfcb
roz3x <oroz3x@gmail.com>
2020-06-19 19:43:15 +0000

    init

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"os"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+type user struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Age string `json:"age"`
+}
+
+var (
+	users = map[int]*user{}
+	seq   = 1
+)
+
+
+func createUser(c echo.Context) error {
+	u := &user{
+		ID: seq,
+	}
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	users[u.ID] = u
+	seq++
+	return c.JSON(http.StatusCreated, u)
+}
+
+func getUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func updateUser(c echo.Context) error {
+	u := new(user)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	users[id].Name = u.Name
+	users[id].Email = u.Email
+	users[id].Age = u.Age
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func deleteUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	delete(users, id)
+	return c.NoContent(http.StatusNoContent)
+}
+
+func main() {
+	e := echo.New()
+	lf ,_ := os.Create("log")
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+	Output:lf,
+}))
+	e.Use(middleware.Recover())
+	e.File("/","front/index.html")
+	e.GET("/data", showAllUsers)
+	e.POST("/users", createUser)
+	e.GET("/users/:id", getUser)
+	e.PUT("/users/:id", updateUser)
+	e.DELETE("/users/:id", deleteUser)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func showAllUsers(c echo.Context) error {
+	allusers := []user{}
+	for _,i:=range users{
+		allusers = append(allusers,*i)
+	}
+	return c.JSON(http.StatusOK,allusers)
+}
